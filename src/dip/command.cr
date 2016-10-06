474179ec1c3f48adb188c05c4823b157dda94416
Michail Merkushin <merkushin.m.s@gmail.com>
2016-10-06 11:22:30 +0500

    Fix executing command with argv

--- src/dip/command.cr
+++ src/dip/command.cr
@@ -1,7 +1,11 @@
 module Dip
   class Command < ::Cli::Command
     def exec!(command : String, argv = nil)
-      system(command, argv) || error!(code: $?.exit_code)
+      command = "#{command} #{argv.join(' ')}" if argv.is_a?(Array)
+
+      puts command.inspect if ENV["DIP_DEBUG"]?
+
+      system(command) || error!(code: $?.exit_code)
     end
   end
 end
