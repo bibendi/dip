9f5348bbcc808ff8b4592592886540c862165804
bibendi <merkushin.m.s@gmail.com>
2018-08-06 18:05:24 +0500

    Fix broken tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,13 +1,17 @@
 require "spec"
 require "../src/dip"
 
-# Remove env vars because var from ENV can broke tests,
-# for example in travis RAILS_ENV = test by default
-ENV.delete("RUBY")
-ENV.delete("RAILS_ENV")
-
 ENV["CRYSTAL_CLI_ENV"] = "test"
 ENV["DIP_ENV"] = "test"
+
+Spec.before_each do
+  # Remove env vars because var from ENV can broke tests,
+  # for example in travis RAILS_ENV = test by default
+  ENV.delete("RUBY")
+  ENV.delete("RAILS_ENV")
+
+  Dip.reset!
+end
 
 def with_dip_file(file_path = "./spec/app/dip.yml")
   ENV["DIP_FILE"] = file_path
