34c895e5ef64c9010871bbdcaadecdaa8b9175a5
bibendi <merkushin.m.s@gmail.com>
2018-08-20 10:29:06 +0500

    Pass first arg vars to compose-run as environment vars
    Example:
    
    `dip MY_VAR=ok run bash -c env | grep MY_VAR`

--- spec/dip/commands/run_spec.cr
+++ spec/dip/commands/run_spec.cr
@@ -9,11 +9,29 @@ describe Dip::Cli::Commands::Run do
       end
     end
 
+    it "runs command with hard env vars" do
+      Dip.env.hard_vars = {"MY_VAR" => "OK"}
+      Dip::Cli::Commands::Run.run(%w(rake)) do |cmd|
+        Dip.env.vars.should eq({"RUBY" => "2.3.1", "RAILS_ENV" => "development"})
+        cmd.out.gets_to_end.should contain("compose run --rm -e MY_VAR=OK app bundle exec rake")
+      end
+      Dip.env.hard_vars.clear
+    end
+
     it "runs subcommand" do
       Dip::Cli::Commands::Run.run(%w(rails c)) do |cmd|
         Dip.env.vars.should eq({"RUBY" => "2.3.1", "RAILS_ENV" => "development"})
         cmd.out.gets_to_end.should contain("compose run --rm app bundle exec rails c")
       end
+    end
+
+    it "runs subcommand with hard env vars" do
+      Dip.env.hard_vars = {"MY_VAR" => "OK"}
+      Dip::Cli::Commands::Run.run(%w(rails c)) do |cmd|
+        Dip.env.vars.should eq({"RUBY" => "2.3.1", "RAILS_ENV" => "development"})
+        cmd.out.gets_to_end.should contain("compose run --rm -e MY_VAR=OK app bundle exec rails c")
+      end
+      Dip.env.hard_vars.clear
     end
 
     it "runs command with specific file" do
